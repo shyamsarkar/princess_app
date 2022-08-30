01f72a05e31971cfd420aa29685ca169505a35ce
Shyam Sarkar <amisam15sarkar05@gmail.com>
2022-08-30 21:59:13 +0530

    config corrected

--- config.py
+++ config.py
@@ -1,10 +1,12 @@
 from operator import and_
 from flask import *
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import or_, and_
+from sqlalchemy import or_, and_, text
+
 from datetime import timedelta, datetime
 import os
 from sqlalchemy.sql import func
+from sqlalchemy.sql import column, table, select
 # flask login
 from flask_login import LoginManager, UserMixin, login_user, logout_user, current_user, login_required
 # flask wtf
@@ -23,9 +25,9 @@ from flask_admin.contrib.sqla import ModelView
 # app Configuration
 app = Flask(__name__, template_folder='templates', static_folder='static')
 
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:@localhost/princess_app'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:123@localhost/princess_app'
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:123@localhost/princess_app'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://wpcmhuddtrvcdo:fdc9b774f26e3012e8734d51d58adfee5a1e3007337cd5f19138c970a3a2b73d@ec2-34-234-240-121.compute-1.amazonaws.com:5432/dc86t460v6iiu8'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
 app.config['SQLALCHEMY_RECORD_QUERIES'] = False
@@ -46,7 +48,7 @@ login_manager.login_message = "Please Enter Username and Password!"
 
 
 # Flask Migrate
-migrate = Migrate(app, db)
+migrate = Migrate(app, db, compare_type=True, render_as_batch=True)
 
 
 """ Flask Admin """
